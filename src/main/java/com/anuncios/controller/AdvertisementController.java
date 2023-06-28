f96c0558daf2df47d2f282c83201727d1508051c
Gregory <gregory.padron@klar.mx>
2023-06-28 19:28:10 -0400

    My change

--- src/main/java/com/anuncios/controller/AdvertisementController.java
+++ src/main/java/com/anuncios/controller/AdvertisementController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/")
 public class AdvertisementController {
+	// Controller
     @Autowired
     private CategoryService categoryService;
 
--- src/main/java/com/anuncios/controller/ScrapeController.java
+++ src/main/java/com/anuncios/controller/ScrapeController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("advertisement")
 public class ScrapeController {
+	
     @Autowired
     private ScraperService scraperService;
 
